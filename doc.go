3a1018da15b2c1b559d28088adb7ed6dd6fcd13b
chenhongji <hongjichen@rastar.com>
2020-03-05 18:36:03 +0800

    read pool(1)

--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 /*
 Package redis implements a Redis client.
+
+related article/blog url list are as blow(for our to comprehend implementation)：
+1、 https://www.jianshu.com/p/c2dd0eb7a5ea
+2、
 */
 package redis
